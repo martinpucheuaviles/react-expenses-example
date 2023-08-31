c95aa7dc371065f7d2469cca2ac528e90d506a48
Martín P. Avilés <89973390+martinpucheuaviles@users.noreply.github.com>
2023-08-31 19:09:03 +0000

    assignment 1 fix

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -5,7 +5,7 @@ function ExpenseDate(props){
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();    
     
-    return <div classeName="expense-date">
+    return <div className="expense-date">
         <div className="expense-date__month">{month}</div>
         <div className="expense-date__year">{year}</div>
         <div className="expense-date__day">{day}</div>
